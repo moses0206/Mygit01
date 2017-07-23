68297343c49f1013113201da7993a189ce093855
moses0206 <18960898@qq.com>
2017-07-23 21:30:01 +0800

    add 461

---
+++ LeetCode_Problems/algorithms/461.Hamming_Distance_v1.c
@@ -0,0 +1,17 @@
+
+int hammingDistance(int x, int y) {
+    int z = x ^ y;
+    int num = 0;
+    int a;
+    for (a = 30; a >= 0 ; a--) {
+        if ( z < (1<<a))continue;
+        else {
+            z-=(1<<a);
+            num++;
+        }
+    }
+    return num;
+}
+
+void main(void) {
+}
